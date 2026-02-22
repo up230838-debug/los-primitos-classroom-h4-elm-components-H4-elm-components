229b7878174281bde81861407685b36ce13598c6
up230838-debug <up230838@alumnos.upa.edu.mx>
2026-02-22 17:31:22 -0600

    Update Helper.elm

--- src/Helper.elm
+++ src/Helper.elm
@@ -1,11 +1,37 @@
 module Helper exposing (..)
 
-import Html
-import Html.Attributes
+import Char
+import Html exposing (..)
+import Html.Attributes exposing (..)
 
 
+joinWords : String -> String -> String
+joinWords word1 word2 =
+    word1 ++ word2
 
--- Puedes usar una definición como la siguiente para probar y visualizar tus resultados, solo debes definir "headers" y "hyperlink"
---main : Html.Html msg
---main =
---    Html.div [] [ headers "Titulos", hyperlink "https://upa.edu.mx" "My School" ]
+
+isUpperChars : List Char -> List Bool
+isUpperChars list =
+    List.map Char.isUpper list
+
+
+evalChars : List Char -> (Char -> Bool) -> List Bool
+evalChars list funTrans =
+    List.map funTrans list
+
+
+headers : String -> Html msg
+headers param =
+    div []
+        [ h1 [] [ text param ]
+        , h2 [] [ text param ]
+        , h3 [] [ text param ]
+        , h4 [] [ text param ]
+        , h5 [] [ text param ]
+        , h6 [] [ text param ]
+        ]
+
+
+hyperlink : String -> String -> Html msg
+hyperlink url label =
+    a [ href url ] [ text label ]
